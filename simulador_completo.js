5692da6854399b1b8f76eaf8124ed5f31142e568
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-05-06 18:40:40 -0500

    Agregar navegación entre secciones y función guardarTasa

--- simulador_completo.js
+++ simulador_completo.js
@@ -9,5 +9,27 @@
   let plazoCalculado = 0;
   let creditoAprobado = false;
 
-  
+  function ocultarSecciones(){
+    document.getElementById("parametros").classList.remove("activa")
+    document.getElementById("clientes").classList.remove("activa")
+  }
+
+  function mostrarSeccion (id){
+    ocultarSecciones()
+    document.getElementById(id).classList.add("activa")
+  }
+
+  function guardarTasa(){
+    let valorTasa = recuperarInt("tasaInteres");
+    if(valorTasa > 20){
+        document.getElementById("mensajeTasa").innerText = "La tasa debe estar entre 10% y 20%";
+    } else if(valorTasa < 10){
+        document.getElementById("mensajeTasa").innerText = "La tasa debe estar entre 10% y 20%";
+    } else {
+        tasaInteres = valorTasa;
+        document.getElementById("mensajeTasa").innerText = "Tasa configurada correctamente: " + valorTasa + "%";
+    }
+}
+
+
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios

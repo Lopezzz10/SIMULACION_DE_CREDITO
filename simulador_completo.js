a3c6082e77db88db0a10098303cd57712fe43e10
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-05-06 19:50:40 -0500

    Agregar guardarCliente y pintarClientes

--- simulador_completo.js
+++ simulador_completo.js
@@ -1,29 +1,28 @@
+let clientes = [];
+let creditos = [];
 
-  let clientes = [];
-  let creditos = [];
+let tasaInteres = 15;
+let clienteSeleccionado = null;
+let cuotaCalculada = 0;
+let montoCalculado = 0;
+let plazoCalculado = 0;
+let creditoAprobado = false;
 
-  let tasaInteres = 15;
-  let clienteSeleccionado = null;
-  let cuotaCalculada = 0;
-  let montoCalculado = 0;
-  let plazoCalculado = 0;
-  let creditoAprobado = false;
+function ocultarSecciones() {
+    document.getElementById("parametros").classList.remove("activa");
+    document.getElementById("clientes").classList.remove("activa");
+}
 
-  function ocultarSecciones(){
-    document.getElementById("parametros").classList.remove("activa")
-    document.getElementById("clientes").classList.remove("activa")
-  }
+function mostrarSeccion(id) {
+    ocultarSecciones();
+    document.getElementById(id).classList.add("activa");
+}
 
-  function mostrarSeccion (id){
-    ocultarSecciones()
-    document.getElementById(id).classList.add("activa")
-  }
-
-  function guardarTasa(){
+function guardarTasa() {
     let valorTasa = recuperarInt("tasaInteres");
-    if(valorTasa > 20){
+    if (valorTasa > 20) {
         document.getElementById("mensajeTasa").innerText = "La tasa debe estar entre 10% y 20%";
-    } else if(valorTasa < 10){
+    } else if (valorTasa < 10) {
         document.getElementById("mensajeTasa").innerText = "La tasa debe estar entre 10% y 20%";
     } else {
         tasaInteres = valorTasa;
@@ -31,5 +30,39 @@
     }
 }
 
+function guardarCliente() {
+    let cedula   = recuperarTexto("cedula");
+    let nombre   = recuperarTexto("nombre");
+    let apellido = recuperarTexto("apellido");
+    let ingresos = recuperarFloat("ingresos");
+    let egresos  = recuperarFloat("egresos");
 
-//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+    let cliente = {
+        cedula:   cedula,
+        nombre:   nombre,
+        apellido: apellido,
+        ingresos: ingresos,
+        egresos:  egresos
+    };
+
+    clientes.push(cliente);
+    pintarClientes();
+}
+
+function pintarClientes() {
+    let tbody = document.getElementById("tablaClientes");
+    tbody.innerHTML = "";
+
+    for (let i = 0; i < clientes.length; i++) {
+        let c = clientes[i];
+        let fila = "<tr>" +
+            "<td>" + c.cedula   + "</td>" +
+            "<td>" + c.nombre   + "</td>" +
+            "<td>" + c.apellido + "</td>" +
+            "<td>" + c.ingresos + "</td>" +
+            "<td>" + c.egresos  + "</td>" +
+            "<td><button onclick=\"seleccionarCliente('" + c.cedula + "')\">Actualizar</button></td>" +
+        "</tr>";
+        tbody.innerHTML += fila;
+    }
+}
